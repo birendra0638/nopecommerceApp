74f735617dfbd5e460f04b61c52d0976d0fcd909
birendra <bindhanibk@gmail.com>
2021-09-07 23:40:49 +0530

    added in test.py

--- testCases/test.py
+++ testCases/test.py
@@ -1,1 +1,10 @@
 # file updated  with comments
+from utilities.customLogger import LogGen
+from utilities.readProperties import ReadConfig
+
+
+class Test_003_AddCustomer:
+    baseURL = ReadConfig.getApplicationURl()
+    userName = ReadConfig.getUserEmail()
+    password = ReadConfig.getPassword()
+    logger = LogGen.loggen()
